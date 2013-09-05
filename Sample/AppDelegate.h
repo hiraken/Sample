327168cacaf27ac06c5f4d11f46a53a32ada5044
hiraken <htb@mvf.biglobe.ne.jp>
2013-09-05 11:08:04 +0900

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by HIRAMATSU KENJIRO on 2013/09/05.
+//  Copyright (c) 2013年 HIRAMATSU KENJIRO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by HIRAMATSU KENJIRO on 2013/09/05.
+//  Copyright (c) 2013年 HIRAMATSU KENJIRO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
